51f221cca6aefd0db47e8a97433c5fe1ea8733e7
nopupu <apple912162@gmail.com>
2024-04-11 14:22:41 +0800

    edit public URL

--- src/index.js
+++ src/index.js
@@ -8,23 +8,23 @@ import store from './redux/store';
 
 
 import { Home, Ticket, Tickets, NewTicket, Calendar, Bookmark, Login, Register, Checkout, PageNotFound } from "./pages"
-
+const publicUrl = (path) => `${process.env.PUBLIC_URL}${path}`;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ticket" element={<Tickets />} />
-        <Route path="/ticket/:id" element={<Ticket />} />
-        <Route path="/newticket" element={<NewTicket />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/bookmark" element={<Bookmark />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/ticket/*" element={<PageNotFound />} />
+        <Route path={publicUrl('/')} element={<Home />} />
+        <Route path={publicUrl('/ticket')} element={<Tickets />} />
+        <Route path={publicUrl('/ticket/:id')} element={<Ticket />} />
+        <Route path={publicUrl('/newticket')} element={<NewTicket />} />
+        <Route path={publicUrl('/calendar')} element={<Calendar />} />
+        <Route path={publicUrl('/bookmark')} element={<Bookmark />} />
+        <Route path={publicUrl('/login')} element={<Login />} />
+        <Route path={publicUrl('/register')} element={<Register />} />
+        <Route path={publicUrl('/checkout')} element={<Checkout />} />
+        <Route path={publicUrl('*')} element={<PageNotFound />} />
+        <Route path={publicUrl('/ticket/*')} element={<PageNotFound />} />
       </Routes>
     </Provider>
   </BrowserRouter>
