87a802fd26a3339545d5555864df00359948af2d
nopupu <apple912162@gmail.com>
2024-04-13 21:55:11 +0800

    add googleLogin and remove register

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,5 @@ export { default as NewTicket } from './NewTicket';
 export { default as Calendar} from './Calendar';
 export { default as Bookmark } from './Bookmark';
 export { default as Login } from './Login';
-export { default as Register } from './Register';
 export { default as Checkout } from './Checkout';
 export { default as PageNotFound } from './PageNotFound';
