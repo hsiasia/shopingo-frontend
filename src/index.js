808cb194e12baf2802a319e66cc90ae4d6a9050e
蔡見昇 <jason_tsai@p225.g4.ntu.edu.tw>
2024-04-14 01:20:20 +0800

    modify: delete public_url

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import {
   PageNotFound,
 } from "./pages";
 
-const baseurl = process.env.PUBLIC_URL;
+const baseurl = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : "/";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId={process.env.client_id}>
