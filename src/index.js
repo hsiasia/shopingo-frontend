2728bb9634f4791d05f911ee0628a8cf584f1d1e
蔡見昇 <jason_tsai@p225.g4.ntu.edu.tw>
2024-04-14 00:58:45 +0800

    fix: modify baseurl to /shopinggo

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
+import {
+  Home,
+  Ticket,
+  Tickets,
+  NewTicket,
+  Calendar,
+  Bookmark,
+  Login,
+  Register,
+  Checkout,
+  PageNotFound,
+} from "./pages";
 
-import { Home, Ticket, Tickets, NewTicket, Calendar, Bookmark, Login, Register, Checkout, PageNotFound } from "./pages"
-const publicUrl = (path) => `${process.env.PUBLIC_URL}${path}`;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const baseurl = process.env.PUBLIC_URL;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId={process.env.client_id}>
-  <BrowserRouter>
-    <Provider store={store}>
-      <Routes>
-        <Route path={publicUrl('/')} element={<Home />} />
-        <Route path={publicUrl('/ticket')} element={<Tickets />} />
-        <Route path={publicUrl('/ticket/:id')} element={<Ticket />} />
-        <Route path={publicUrl('/newticket')} element={<NewTicket />} />
-        <Route path={publicUrl('/calendar')} element={<Calendar />} />
-        <Route path={publicUrl('/bookmark')} element={<Bookmark />} />
-        <Route path={publicUrl('/login')} element={<Login />} />
-        <Route path={publicUrl('/checkout')} element={<Checkout />} />
-        <Route path={publicUrl('*')} element={<PageNotFound />} />
-        <Route path={publicUrl('/ticket/*')} element={<PageNotFound />} />
-
-      </Routes>
-    </Provider>
-  </BrowserRouter>
+    <BrowserRouter basename={baseurl}>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ticket" element={<Tickets />} />
+          <Route path="/ticket/:id" element={<Ticket />} />
+          <Route path="/newticket" element={<NewTicket />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/bookmark" element={<Bookmark />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/ticket/*" element={<PageNotFound />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </GoogleOAuthProvider>
 );
