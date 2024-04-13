73f89c4cd97dbf433dd008959ebb531096746045
蔡見昇 <jason_tsai@p225.g4.ntu.edu.tw>
2024-04-14 01:06:56 +0800

    feat: add google signin button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Footer, Navbar } from "../components";
-import { GoogleLogin } from '@react-oauth/google';
-
+import { GoogleLogin } from "react-google-login";
+const clientId =
+  "1043514981991-50nrdq6cst3teco3ft2m36h06r90qsq8.apps.googleusercontent.com";
 const Login = () => {
   const responseMessage = (res) => {
     console.log(res);
@@ -12,22 +13,26 @@ const Login = () => {
     console.log("id_token", res.tokenId);
     const id_token = res.tokenId;
     console.log(id_token);
-};
-const errorMessage = (error) => {
+  };
+  const errorMessage = (error) => {
     console.log(error);
-};
+  };
   return (
     <>
       <Navbar />
       <div className="container my-3 py-3">
         <h1 className="text-center">Login</h1>
-        <hr/>
-          <GoogleLogin 
-            onSuccess={responseMessage} 
-            onError={errorMessage}
-          />
-        <br/>
-        <br/>
+        <hr />
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Login with Google"
+          onSuccess={Login}
+          onFailure={errorMessage}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        />
+        <br />
+        <br />
       </div>
       <Footer />
     </>
