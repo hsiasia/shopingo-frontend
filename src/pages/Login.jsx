11fe3bcd155eb7d83e9fc136e9d251d5186b2b2c
蔡見昇 <jason_tsai@caijianshengdeMacBook-Air.local>
2024-04-14 12:22:03 +0800

    feat: add google login btn

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Footer, Navbar } from "../components";
-import { GoogleLogin } from "react-google-login";
+
 const clientId =
   "1043514981991-50nrdq6cst3teco3ft2m36h06r90qsq8.apps.googleusercontent.com";
+
 const Login = () => {
-  const responseMessage = (res) => {
-    console.log(res);
-    console.log("ID:", res.profileObj.googleId);
-    console.log("Name:", res.profileObj.name);
-    console.log("Image URL:", res.profileObj.imageUrl);
-    console.log("Email:", res.profileObj.email);
-    console.log("id_token", res.tokenId);
-    const id_token = res.tokenId;
-    console.log(id_token);
+  useEffect(() => {
+    window.google?.accounts.id.initialize({
+      client_id: clientId,
+      callback: handleCredentialResponse,
+    });
+    window.google?.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+    window.google?.accounts.id.prompt();
+  }, []);
+
+  const handleCredentialResponse = (response) => {
+    console.log("response", response);
+    console.log("Encoded JWT ID token: " + response.credential);
   };
-  const errorMessage = (error) => {
-    console.log(error);
-  };
+
   return (
     <>
       <Navbar />
       <div className="container my-3 py-3">
         <h1 className="text-center">Login</h1>
         <hr />
-        <GoogleLogin
-          clientId={clientId}
-          buttonText="Login with Google"
-          onSuccess={Login}
-          onFailure={errorMessage}
-          cookiePolicy={"single_host_origin"}
-          isSignedIn={true}
-        />
+        <div id="signInDiv"></div>{" "}
+        {/* Google SignIn button will be rendered here */}
         <br />
         <br />
       </div>
