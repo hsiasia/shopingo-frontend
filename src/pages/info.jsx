2f74b106ef72408aa329e558fa7311ab5c54a880
Wei Lun <b09103015@ntu.edu.tw>
2024-05-01 16:09:23 +0800

    completing tables in page_info

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
-import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import React from "react";
+import { Accordion, AccordionSummary, AccordionDetails, Typography, Button} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
 
 import {
   Footer,
@@ -9,35 +8,59 @@ import {
 } from "../components"; //baisc website components
 
 interface InfoProps{
-  title: String;
-  content: JSX.Element;
+  BarTitle: String;
+  Title:  String;
+  Date: String;
+  Location: String;
+}
+
+const styles = {
+  tableHeader: {
+    border: '1px solid #dddddd',
+    padding: '8px',
+    textAlign: 'left',
+    backgroundColor: '#f2f2f2',
+  },
+  tableCell: {
+    border: '1px solid #dddddd',
+    padding: '8px',
+    textAlign: 'left',
+  },
 }
 
 const Info = () => {
-  /*const bars = [
-    "My Events",
-    "Future Events",
-    "Saved Events",
-    "Finished Events",
-  ];
-  let [selectedIndex, setSelectedIndex] = useState(-1);
-  
-  <ul className="list-group">
-  {bars.map((item, index)=> <li className={selectedIndex === index ? "list-group-item active":"list-group-item"} onClick={() => {setSelectedIndex(index);}} key={item}>{item}</li>)}
-  </ul>*/
 
   function ProductAccordion (props: InfoProps) {
     return (
-      <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>{ props.title }</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            <strong>{ props.content }</strong>
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <typography>{props.BarTitle}</typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+            <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+              <thead>
+                <tr>
+                  <th style={styles.tableHeader}>Event</th>
+                  <th style={styles.tableHeader}>Date</th>
+                  <th style={styles.tableHeader}>Location</th>
+                  <th style={styles.tableHeader}>View</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style={styles.tableCell}>{props.Title}</td>
+                  <td style={styles.tableCell}>{props.Date}</td>
+                  <td style={styles.tableCell}>{props.Location}</td>
+                  <td style={styles.tableCell}>
+                    <Button variant="contained" href="">View</Button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
     );
   };
 
@@ -47,10 +70,10 @@ const Info = () => {
       <div className="container my-3 py-3">
         <h1 className="text-center">Personal Info</h1>
         <hr />
-        <ProductAccordion title="My Event" content={<strong>Here are the events you've created</strong>}/>
-        <ProductAccordion title="Incoming Event" content={<strong>Here are the events you will be paricipating</strong>}/>
-        <ProductAccordion title="Saved Event" content={<strong>Here are the events you are interested in</strong>}/>
-        <ProductAccordion title="Finished Event" content={<strong>Here are the events you've attend or expired</strong>}/>
+        <ProductAccordion BarTitle="My Events" Title="BOGOF" Date="2002-01-05" Location="Taipei"/>
+        <ProductAccordion BarTitle="Incoming Events" Title="BOGOF" Date="2002-01-05" Location="Taipei"/>
+        <ProductAccordion BarTitle="Saved Events" Title="BOGOF" Date="2002-01-05" Location="Taipei"/>
+        <ProductAccordion BarTitle="Finished Events" Title="BOGOF" Date="2002-01-05" Location="Taipei"/>
         <hr />
       </div>
       <Footer />
