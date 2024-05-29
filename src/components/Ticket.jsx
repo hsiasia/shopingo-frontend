f2bbe0d47bd831e3df0ba7c39e6eff8bf6f6a8d8
nopupu <apple912162@gmail.com>
2024-05-29 11:12:37 +0800

    fix Ticket_api forbidden

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -167,7 +167,7 @@ const Ticket = ({ticket, defaultExpanded}) => {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        //'Authorization': `Bearer ${localStorage.getItem('auth_token')}`
+        'Authorization': `Bearer ${localStorage.getItem('auth_token')}`
       },
     })
     .then(response => {
